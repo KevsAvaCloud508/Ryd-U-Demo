8ed085c86365870c9eaab11fcedf7316a6fc134e
N1m33 <up230442@alumnos.upa.edu.mx>
2026-08-02 16:51:00 -0600

    Dashboard Listo

--- frontend/src/features/driver/pages/RequestsPage.tsx
+++ frontend/src/features/driver/pages/RequestsPage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { ActionButtons, Avatar, PageHeader, StatusBadge } from '../../../shared/components';
 
